64115b787be6bfb4f00d5f4a3befd7d577e06a99
suprabhatkr <jnvsuprabh@gmail.com>
2021-04-21 22:50:16 +0530

    JavaFileStructure

---
+++ OOPS/JavaFileStructure/anynameCorrect.java
@@ -0,0 +1,10 @@
+class A{
+	public static void main(){
+		System.out.println("helloA");
+	}
+}
+class B{
+	public static void main(){
+		System.out.println("helloB");
+	}
+}
---
+++ OOPS/JavaFileStructure/publicClassName.java
@@ -0,0 +1,10 @@
+class A{
+	public static void main(String[] args){
+		System.out.println("HelloA");
+	}
+}
+public class publicClassName{
+	public static void main(String[] args){
+		System.out.println("Hello Public");
+	}
+}
